13d206fddf9de999068b6c6d83b5c561b1efe9e8
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-27 20:00:34 +0200

    raccolta delle pagine in array + display in ul

--- laravel-primi-passi/resources/views/home.blade.php
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -1,3 +1,25 @@
+@php
+    $pages = [
+        [
+            'name' => 'Privacy & Policies',
+            'path' => '/privacy',
+        ],
+        [
+            'name' => 'Contact Us',
+            'path' => '/contact',
+        ],
+        [
+            'name' => 'About Us',
+            'path' => '/about',
+        ],
+        [
+            'name' => 'Impostazioni',
+            'path' => '/settings',
+        ],
+    ];
+
+@endphp
+
 <!DOCTYPE html>
 <html lang="it">
 
@@ -9,6 +31,16 @@
 </head>
 
 <body>
+
+    <h1>Questa è l'homepage</h1>
+
+    <ul class="page-links">
+
+        @foreach ($pages as $page)
+            <li> <a href="{{ $page['path'] }}">{{ $page['name'] }}</a> </li>
+        @endforeach
+
+    </ul>
 
 </body>
 
